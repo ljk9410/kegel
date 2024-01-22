5124ff5bfe2cb1e43a7eb29ab493979a447fb2d2
ljk9410 <dkssud6115@gmail.com>
2024-01-22 16:51:55 +0900

    동영상 안나오는 문제

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./MainContent.css";
 import useIsMobileView from "../hooks/useIsMobileView";
-import kegelVideo from "../assets/videos/kegel_sticky.mp4";
 
 const MainContent = () => {
   const isMobileView = useIsMobileView();
@@ -15,8 +14,7 @@ const MainContent = () => {
         muted={true}
         loop={true}
         playsInline={true}
-        controls={true}
-        src={kegelVideo}
+        src={`${process.env.PUBLIC_URL}/videos/kegel_sticky.mp4`}
         type="video/mp4"
       />
       <a className="madeBy" href={"https://planfit.sng.link/Al6s7/b4fm/wf79"}>
